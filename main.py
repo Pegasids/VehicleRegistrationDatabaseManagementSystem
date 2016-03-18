ab146735a8c7c201d4a43010b11d4060bdf9b4d5
TheAspiredOne <altan@ualberta.ca>
2016-03-17 20:43:40 -0600

    main file inclusion statements

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import sys
 import cx_Oracle # the package used for accessing Oracle in Python
 import getpass # the package for getting password from user without displaying it
+from addperson_nathan import *
+from auto_transaction import *
 from avery import*
 from canopus import *
 from getting_the_damn_photo import*
+from vehicle_registration_canopus import *
+
 
 def applications(curs,connection):                                                                        # starts application programs
     exit_code = False
