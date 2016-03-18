1905f55ed4222717d48c0d2ba016ff7791944626
TheAspiredOne <altan@ualberta.ca>
2016-03-17 21:14:23 -0600

    combinign

--- auto_transaction_nathan.py
+++ auto_transaction_nathan.py
@@ -4,7 +4,7 @@
 import sys
 import cx_Oracle # the package used for accessing Oracle in Python
 import getpass # the package for getting password from user without displaying it
-from addperson import *
+from addperson_nathan import *
 #-------------------------------------------Auto Transaction----------------------------------------------------
 def AutoTransaction(curs, connection):
 	# executing queries and get data
